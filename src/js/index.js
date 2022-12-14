1095ba6312789e457d481d546e52ac3acd6356c1
LindseaMartin <ninzerb@gmail.com>
2022-12-14 14:59:08 -0700

    finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,19 @@
-async function getToDos() {
-  const res = await fetch('/.netlify/functions/todos');
-  const data = await res.json();
-  console.log(data);
+import {categoryTemplates} from './templates/categories'
+
+//  init app, load data, loop data, create the todos and them to the DOM.
+
+async function appInit(){
+   const res = await fetch('.netlify/functions/todos')
+   const todos = await res.json()
+   const containerElement = document.createElement('div')
+   let markup = ``
+   todos.forEach(todo =>{
+         const template = categoryTemplates[todo.category](todo)
+         markup += template
+         
+   })
+   containerElement.innerHTML = markup
+   document.querySelector('main').append(containerElement)
 }
 
-getToDos();
+appInit()
